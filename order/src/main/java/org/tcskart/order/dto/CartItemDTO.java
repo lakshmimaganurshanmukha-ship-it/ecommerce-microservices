07a163163b3d33b1ef81ae2495fb60492f1f6786
lakshmimaganurshanmukha-ship-it <lakshmimaganurshanmukha@gmail.com>
2026-01-18 15:36:37 +0530

    Update CartItemDTO.java

--- order/src/main/java/org/tcskart/order/dto/CartItemDTO.java
+++ order/src/main/java/org/tcskart/order/dto/CartItemDTO.java
@@ -9,12 +9,7 @@ public class CartItemDTO {
 	    private Long productId;
 	    private Integer quantity;
 	    private Long amount;
-//		private
-//		public CartItemDTO(Integer productId, Integer quantity, Integer amount) {
-////			super();
-//			this.productId = productId;
-//			this.quantity = quantity;
-//			this.amount = amount;
+
 //		}
 	}
 
